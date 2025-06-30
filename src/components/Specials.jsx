b33aa317be7ef8637e62ce79ada259ae59731d53
jeremiahmiranda79 <jeremiahmiranda79@gmail.com>
2025-06-29 19:45:59 -0700

    fixed stuff

--- src/components/Specials.jsx
+++ src/components/Specials.jsx
@@ -8,17 +8,20 @@ const Specials = () => {
       <h1 rel="preload" style={{textAlign: 'center'}}>Specials</h1>
       <Information/>
       <p className='y' style={{textAlign: 'center', fontWeight: 'bold'}}>*Ask us about our all day Specials*</p>
-      <h2 style={{textAlign: 'center'}}>Try Our New Creation</h2>
+      
+      {/* <h2 style={{textAlign: 'center'}}>Try Our New Creation</h2>
       <p style={{textAlign: 'center'}}>
         <img src="/images/specials/pasta-bowl.webp" alt="pasta bowl" className="Image"/>
-      </p>
-      <p className='x' style={{textAlign: 'center', marginBottom: '0px', fontWeight: 'bold'}}>Check in on our new pasta bowl</p>
+      </p> */}
+
+      {/* <p className='x' style={{textAlign: 'center', marginBottom: '0px', fontWeight: 'bold'}}>Check in on our new pasta bowl</p>
       <p className='x' style={{textAlign: 'center', marginBottom: '0px'}}>$14.99 plus tax</p>
-      <p className='y' style={{textAlign: 'center', marginBottom: '18px'}}>Choice of spaghetti or penne. <br/> Choice of chicken, meatball, or sausage. <br/> Choice of marinara, pink vodka, or alfredo. <br/> Topped with fresh basil, and parmesan</p>
+      <p className='y' style={{textAlign: 'center', marginBottom: '18px'}}>Choice of spaghetti or penne. <br/> Choice of chicken, meatball, or sausage. <br/> Choice of marinara, pink vodka, or alfredo. <br/> Topped with fresh basil, and parmesan</p> */}
       <h2 style={{textAlign: 'center', marginTop: '10px'}}>Game Day</h2>
       <p style={{textAlign: 'center'}}>
         <img src="/images/specials/sunday-football.webp" alt="pasta bowl" className="Image"/>
       </p>
+
       <p className='x' style={{textAlign: 'center'}}>Large 16" one topping Neapolitan pizza & 10 wings for $29.99 plus tax.</p>
       <p className='x' style={{textAlign: 'center'}}>Available for dine in or take out, open to close on NFL game day's only(Sunday, Monday, Thursday).</p>
       <h2 style={{textAlign: 'center', marginTop: '10px'}}>Lunch Specials</h2>
@@ -32,6 +35,7 @@ const Specials = () => {
         <p className='x' style={{marginBottom: '0px'}}>6 inch Cold Sub w/ Fries</p>
         <p className='x' style={{marginBottom: '0px'}}>$11.99</p>
       </div>
+      
       <h2 style={{textAlign: 'center', marginTop: '10px'}}>Pizza Topping Specials</h2>
       <div style={{textAlign: 'center', marginTop: '10px'}}>
         <p className='x' style={{marginBottom: '0px'}}>All day every day</p>
